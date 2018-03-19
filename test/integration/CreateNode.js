32bb194bf2d784c540277ae167ce62c55788f49a
Lukas Hechenberger <admin@ls-age.com>
2018-03-19 11:06:28 +0100

    test: Add basic integration test

---
+++ test/integration/CreateNode.js
@@ -0,0 +1,38 @@
+import test from 'ava';
+import NodeId from 'atscm/out/lib/model/opcua/NodeId';
+import { NodeClass, DataType } from 'node-opcua';
+import { serverDirectory as serverDir } from '../../config';
+import { Variant, callScript } from './_helpers';
+
+const script = new NodeId(`ns=1;s=SYSTEM.LIBRARY.ATVISE.SERVERSCRIPTS.${serverDir}.CreateNode`);
+
+function createNode(options) {
+  return callScript(script, {
+    paramObjString: new Variant(DataType.String, JSON.stringify(options)),
+  });
+}
+
+test('igores existing nodes', async t => {
+  const nodeId = new NodeId('ns=1;s=AGENT.OBJECTS');
+  const { createdNode, creatingNodeFailed } = await createNode({
+    nodeId,
+  });
+
+  t.false(createdNode);
+  t.false(creatingNodeFailed);
+});
+
+test('creates atvise server nodes', async t => {
+  const nodeId = new NodeId(`ns=1;s=AGENT.OBJECTS.test-${Date.now()}`);
+  const { createdNode, creatingNodeFailed } = await createNode({
+    nodeClass: NodeClass.Variable.value,
+    nodeId,
+    parentNodeId: nodeId.parent,
+    typeDefinition: new NodeId('ns=1;i=62'),
+    value: 13,
+    dataType: DataType.Integer,
+  });
+
+  t.true(createdNode);
+  t.false(creatingNodeFailed);
+});
---
+++ test/integration/_helpers.js
@@ -0,0 +1,54 @@
+import { StatusCodes, DataType, VariantArrayType as ArrayType } from 'node-opcua';
+import NodeId from 'atscm/out/lib/model/opcua/NodeId.js';
+import Session from 'atscm/out/lib/server/Session.js';
+
+export async function callMethod({ base, method: methodId, args: inputArguments = [] }) {
+  const session = await Session.create();
+
+  return new Promise((resolve, reject) => {
+    session.call([{
+      objectId: base || methodId.parent,
+      methodId,
+      inputArguments,
+    }], (err, [{ statusCode, outputArguments }] = []) => {
+      if (err) {
+        reject(err);
+      } else if (statusCode.value !== StatusCodes.Good.value) {
+        reject(new Error(`Method failed with status ${statusCode.name}.
+(${statusCode.description})`));
+      } else {
+        resolve(outputArguments);
+      }
+    });
+  });
+}
+
+export class Variant {
+
+  constructor(dataType, value, arrayType = ArrayType.Scalar) {
+    this.dataType = dataType;
+    this.value = value;
+    this.arrayType = arrayType;
+  }
+
+}
+
+export async function callScript(script, options = {}) {
+  const [status, description, keys, values] = await callMethod({
+    method: new NodeId(NodeId.NodeIdType.STRING, 'AGENT.SCRIPT.METHODS.callScript', 1),
+    args: [
+      new Variant(DataType.NodeId, script),
+      new Variant(DataType.NodeId, script.parent),
+      new Variant(DataType.String, Object.keys(options), ArrayType.Array),
+      new Variant(DataType.Variant, Object.values(options), ArrayType.Array),
+    ],
+  });
+
+  if (status.value !== StatusCodes.Good) {
+    throw new Error(`Script failed: ${description.value}`);
+  }
+
+  return keys.value.reduce((result, key, i) => Object.assign(result, {
+    [key]: values.value[i].value,
+  }), {});
+}
