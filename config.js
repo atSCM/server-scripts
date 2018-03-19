cb85d5e7a0559d979c3389c4780b64ba7ace988d
Lukas Hechenberger <admin@ls-age.com>
2018-03-19 11:50:17 +0100

    fix(circleci): Use sha for build directory name
    Build number increments for each job in the workflow

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const runningOnCI = Boolean(process.env.CI);
 export const serverDirectory = process.env.SERVER_DIR || (runningOnCI ?
-  `ccibuild-${process.env.CIRCLE_BUILD_NUM || 'local'}` :
+  `ccibuild-${process.env.CIRCLE_SHA1 || 'local'}` :
   'atscm');
